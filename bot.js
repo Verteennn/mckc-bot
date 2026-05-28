7c3fed0642fae81bbc14194da653ef2e8eb59f0d
Verteennn <mustaffa420zs@gmail.com>
2026-05-28 09:18:28 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,18 +1,40 @@
 const mineflayer = require('mineflayer')
 
-const bot = mineflayer.createBot({
-  host: 'MCKC.PL',
-  username: 'SyzyfBot123',
-  version: false
-})
+function startBot() {
+    const bot = mineflayer.createBot({
+      host: 'MCKC.PL',
+      username: 'SyzyfBot123',
+      version: false
+    })
 
-bot.on('spawn', () => {
-  console.log('Bot online')
+    bot.on('spawn', () => {
+      console.log('Bot wszedł na serwer!')
+      setTimeout(() => {
+        bot.chat('/login syzyf123')
+      }, 3000)
+    })
 
-  setTimeout(() => {
-    bot.chat('/login syzyf123')
-  }, 3000)
-})
+    bot.on('message', (jsonMsg) => {
+      console.log(jsonMsg.toString())
+    })
 
-bot.on('error', err => console.log(err))
-bot.on('kicked', reason => console.log(reason))
+    bot.on('kicked', (reason) => {
+      console.log('Wyrzucono z serwera:', reason)
+      // Czekamy 60 sekund i próbujemy ponownie, zamiast zamykać bota
+      setTimeout(startBot, 60000)
+    })
+
+    bot.on('error', (err) => {
+      console.log('Błąd:', err)
+      setTimeout(startBot, 60000)
+    })
+}
+
+startBot()
+
+// Prosty serwer HTTP, żeby Render nie usypiał bota
+const http = require('http');
+http.createServer((req, res) => {
+  res.write('Bot dziala');
+  res.end();
+}).listen(process.env.PORT || 3000);
